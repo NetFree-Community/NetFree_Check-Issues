628d5a6a12a8028ee6468080878d6c2e4a99512f
DS <strohdovid@gmail.com>
2017-03-16 17:01:38 +0200

    Added Files

---
+++ CheckIssues.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace NetFree_Check_Issues
+{
+    partial class CheckIssues
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CheckIssues));
+            this.button1 = new System.Windows.Forms.Button();
+            this.TimeCorrect = new System.Windows.Forms.Label();
+            this.Internet = new System.Windows.Forms.Label();
+            this.Cert = new System.Windows.Forms.Label();
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.ISP = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loader = new System.Windows.Forms.PictureBox();
+            this.layout = new System.Windows.Forms.Panel();
+            this.fixcert = new System.Windows.Forms.Button();
+            this.fixtime = new System.Windows.Forms.Button();
+            this.table.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loader)).BeginInit();
+            this.layout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "בדיקה חוזרת";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TimeCorrect
+            // 
+            this.TimeCorrect.AutoSize = true;
+            this.TimeCorrect.Location = new System.Drawing.Point(81, 81);
+            this.TimeCorrect.Name = "TimeCorrect";
+            this.TimeCorrect.Size = new System.Drawing.Size(83, 13);
+            this.TimeCorrect.TabIndex = 2;
+            this.TimeCorrect.Text = "תאריך לא נכון";
+            // 
+            // Internet
+            // 
+            this.Internet.AutoSize = true;
+            this.Internet.Location = new System.Drawing.Point(61, 0);
+            this.Internet.Name = "Internet";
+            this.Internet.Size = new System.Drawing.Size(103, 13);
+            this.Internet.TabIndex = 3;
+            this.Internet.Text = "אינטרנט לא מחובר";
+            // 
+            // Cert
+            // 
+            this.Cert.AutoSize = true;
+            this.Cert.Location = new System.Drawing.Point(72, 54);
+            this.Cert.Name = "Cert";
+            this.Cert.Size = new System.Drawing.Size(92, 13);
+            this.Cert.TabIndex = 4;
+            this.Cert.Text = "תעודה לא מותקן";
+            // 
+            // table
+            // 
+            this.table.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.table.ColumnCount = 1;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.Controls.Add(this.Internet, 0, 0);
+            this.table.Controls.Add(this.ISP, 0, 1);
+            this.table.Controls.Add(this.TimeCorrect, 0, 3);
+            this.table.Controls.Add(this.Cert, 0, 2);
+            this.table.Cursor = System.Windows.Forms.Cursors.Default;
+            this.table.Location = new System.Drawing.Point(31, 95);
+            this.table.Name = "table";
+            this.table.RowCount = 4;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.Size = new System.Drawing.Size(167, 108);
+            this.table.TabIndex = 5;
+            // 
+            // ISP
+            // 
+            this.ISP.AutoSize = true;
+            this.ISP.Location = new System.Drawing.Point(83, 27);
+            this.ISP.Name = "ISP";
+            this.ISP.Size = new System.Drawing.Size(81, 13);
+            this.ISP.TabIndex = 1;
+            this.ISP.Text = "ספק לא מחובר";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(209, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loader
+            // 
+            this.loader.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loader.Image = ((System.Drawing.Image)(resources.GetObject("loader.Image")));
+            this.loader.ImageLocation = "";
+            this.loader.InitialImage = ((System.Drawing.Image)(resources.GetObject("loader.InitialImage")));
+            this.loader.Location = new System.Drawing.Point(12, 5);
+            this.loader.Name = "loader";
+            this.loader.Size = new System.Drawing.Size(33, 33);
+            this.loader.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.loader.TabIndex = 7;
+            this.loader.TabStop = false;
+            this.loader.Visible = false;
+            // 
+            // layout
+            // 
+            this.layout.Controls.Add(this.fixcert);
+            this.layout.Controls.Add(this.fixtime);
+            this.layout.Location = new System.Drawing.Point(31, 95);
+            this.layout.Name = "layout";
+            this.layout.Size = new System.Drawing.Size(460, 107);
+            this.layout.TabIndex = 9;
+            // 
+            // fixcert
+            // 
+            this.fixcert.Location = new System.Drawing.Point(219, 54);
+            this.fixcert.Name = "fixcert";
+            this.fixcert.Size = new System.Drawing.Size(68, 21);
+            this.fixcert.TabIndex = 11;
+            this.fixcert.Text = "התקן כעת";
+            this.fixcert.UseVisualStyleBackColor = true;
+            this.fixcert.Click += new System.EventHandler(this.fixcert_Click);
+            // 
+            // fixtime
+            // 
+            this.fixtime.Location = new System.Drawing.Point(219, 81);
+            this.fixtime.Name = "fixtime";
+            this.fixtime.Size = new System.Drawing.Size(68, 23);
+            this.fixtime.TabIndex = 9;
+            this.fixtime.Text = "תקן כעת";
+            this.fixtime.UseVisualStyleBackColor = true;
+            this.fixtime.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CheckIssues
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 266);
+            this.Controls.Add(this.loader);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.table);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.layout);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CheckIssues";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "בדיקת בעיות BETA";
+            this.Load += new System.EventHandler(this.CheckIssues_Load);
+            this.table.ResumeLayout(false);
+            this.table.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loader)).EndInit();
+            this.layout.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label TimeCorrect;
+        private System.Windows.Forms.Label Internet;
+        private System.Windows.Forms.Label Cert;
+        private System.Windows.Forms.TableLayoutPanel table;
+        private System.Windows.Forms.Label ISP;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox loader;
+        private System.Windows.Forms.Panel layout;
+        private System.Windows.Forms.Button fixcert;
+        private System.Windows.Forms.Button fixtime;
+    }
+}
+
---
+++ CheckIssues.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetFree_Check_Issues
+{
+    public partial class CheckIssues : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetSystemTime(ref SYSTEMTIME st);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEMTIME
+        {
+            public short wYear;
+            public short wMonth;
+            public short wDayOfWeek;
+            public short wDay;
+            public short wHour;
+            public short wMinute;
+            public short wSecond;
+            public short wMilliseconds;
+        }
+
+        public CheckIssues()
+        {
+            InitializeComponent();
+            loader.Size = layout.Size;
+            loader.Location = layout.Location;
+            refresh();
+        }
+
+        private void CheckIssues_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public DateTime InternetTime()
+        {
+            DateTime datetime = DateTime.MinValue;
+            for (int tries = 1; tries < 3; tries++)
+            {
+                try
+                {
+                    const string ntpServer = "time.windows.com";
+
+                    // NTP message size - 16 bytes of the digest (RFC 2030)
+                    var ntpData = new byte[48];
+
+                    //Setting the Leap Indicator, Version Number and Mode values
+                    ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+                    var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+
+                    //The UDP port number assigned to NTP is 123
+                    var ipEndPoint = new IPEndPoint(addresses[0], 123);
+                    //NTP uses UDP
+                    var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                    socket.Connect(ipEndPoint);
+
+                    //Stops code hang if NTP is blocked
+                    socket.ReceiveTimeout = 3000;
+
+                    socket.Send(ntpData);
+                    socket.Receive(ntpData);
+                    socket.Close();
+
+                    //Offset to get to the "Transmit Timestamp" field (time at which the reply 
+                    //departed the server for the client, in 64-bit timestamp format."
+                    const byte serverReplyTime = 40;
+
+                    //Get the seconds part
+                    ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+
+                    //Get the seconds fraction
+                    ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+
+                    //Convert From big-endian to little-endian
+                    intPart = SwapEndianness(intPart);
+                    fractPart = SwapEndianness(fractPart);
+
+                    var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+
+                    //**UTC** time
+                    var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
+                    datetime = networkDateTime;
+                    return datetime;
+                }
+                catch { };
+            }
+            return datetime;
+        }
+
+        private bool TimeRight()
+        {
+            return InternetTime().ToLocalTime().Date == DateTime.Now.Date;
+        }
+
+        private int CheckCert(string isp)
+        {
+            if (isp == "")
+                return -1;
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+
+            store.Open(OpenFlags.ReadOnly);
+
+            foreach (X509Certificate2 Cert in store.Certificates)
+            {
+                if (Cert.Issuer.Contains("NetFree Sign") && Cert.Issuer.Contains(isp))
+                {
+                    try
+                    {
+                        Ping pinger = new Ping();
+                        PingReply gmail = pinger.Send("mail.google.com");
+                        if (gmail.Status == IPStatus.Success)
+                            return 2;
+                    }
+                    catch { };
+                    return 1;
+                }
+            }
+            return 0;
+        }
+
+        private void CertInstall()
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                byte[] CertFile = client.DownloadData("http://netfree.link/netfree-ca.crt"); ;
+                X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+                X509Certificate2 cert = new X509Certificate2();
+                cert.Import(CertFile);
+                store.Open(OpenFlags.ReadWrite);
+                store.Add(cert); //where cert is an X509Certificate object
+                store.Close();
+            }
+            catch { };
+        }
+
+        static uint SwapEndianness(ulong x)
+        {
+            return (uint)(((x & 0x000000ff) << 24) +
+                           ((x & 0x0000ff00) << 8) +
+                           ((x & 0x00ff0000) >> 8) +
+                           ((x & 0xff000000) >> 24));
+        }
+
+        private string MyIsp()
+        {
+            string ISP = "";
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                try
+                {
+                    ISP = client.DownloadString("http://netfree.link/nf/user/0");
+                }
+                catch { };
+
+                if (ISP.Contains("@rl-internet"))
+                    ISP = "RL";
+                else if (ISP.Contains("@019"))
+                    ISP = "019";
+                else if (ISP.Contains("@URI"))
+                    ISP = "NISIM-VPN";
+                else if (ISP.Contains("@netfree-anywhere"))
+                    ISP = "NetFreeAnywhere";
+                else
+                    ISP = "";
+            }
+            return ISP;
+        }
+
+        private int Connected()
+        {
+            Ping pinger = new Ping();
+            for (int tries = 1; tries < 3; tries++)
+            {
+                try
+                {
+                    PingReply netfree = pinger.Send("NetFree.Link");
+                    PingReply gmail = pinger.Send("mail.google.com");
+                    if (netfree.Status == IPStatus.Success)
+                        return 2;
+                    else if (gmail.Status == IPStatus.Success)
+                        return 1;
+                }
+                catch { };
+            }
+            return 0;
+        }
+
+        private void SetTime(DateTime datetime)
+        {
+            SYSTEMTIME st = new SYSTEMTIME();
+            st.wYear = short.Parse(datetime.Year.ToString());
+            st.wMonth = short.Parse(datetime.Month.ToString());
+            st.wDay = short.Parse(datetime.Day.ToString());
+            st.wHour = short.Parse(datetime.Hour.ToString());
+            st.wMinute = short.Parse(datetime.Minute.ToString());
+            st.wSecond = short.Parse(datetime.Second.ToString());
+
+            SetSystemTime(ref st);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void refresh()
+        {
+            fixcert.Visible = false;
+            fixtime.Visible = false;
+            loader.Visible = true;
+            if (Connected() == 2)
+            {
+                string isp = MyIsp();
+                Cert.Visible = true;
+                ISP.Visible = true;
+                TimeCorrect.Visible = true;
+                Internet.Text = "אינטרנט מחובר";
+                if (CheckCert(isp) == 2)
+                    Cert.Text = "תעודה מותקן";
+                else if (CheckCert(isp) == 1)
+                {
+                    Cert.Text = "תעודה לא עובד";
+                    fixcert.Visible = true;
+                }
+                else if (CheckCert(isp) == 0)
+                {
+                    Cert.Text = "תעודה לא מותקן";
+                    fixcert.Visible = true;
+                }
+                else
+                    Cert.Text = "אין ספק נטפרי";
+
+                if (isp != "")
+                    ISP.Text = isp;
+                else
+                    ISP.Text = "לא מחובר לספק נטפרי";
+
+                if (TimeRight())
+                    TimeCorrect.Text = "תאריך נכון";
+                else
+                {
+                    TimeCorrect.Text = "תאריך לא נכון";
+                    fixtime.Visible = true;
+                }
+            }
+            else if (Connected() == 1)
+            {
+                TimeCorrect.Visible = true;
+                Internet.Text = "אין גישה לנטפרי";
+                Cert.Visible = false;
+                ISP.Visible = false;
+
+                if (TimeRight())
+                    TimeCorrect.Text = "תאריך נכון";
+                else
+                {
+                    TimeCorrect.Text = "תאריך נכון";
+                    fixtime.Visible = true;
+                }
+            }
+            else
+            {
+                Internet.Text = "אינטרנט לא מחובר";
+                Cert.Visible = false;
+                ISP.Visible = false;
+                TimeCorrect.Visible = false;
+            }
+            loader.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetTime(InternetTime());
+            refresh();
+            Process.Start("timedate.cpl").WaitForExit();
+            refresh();
+        }
+
+        private void fixcert_Click(object sender, EventArgs e)
+        {
+            CertInstall();
+            refresh();
+            if (CheckCert(MyIsp()) == 1 || CheckCert(MyIsp()) == 2)
+            {
+                MessageBox.Show("התעודה הותקן בהצלחה", "התקנת תעודת אבטחה", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                DialogResult answer = MessageBox.Show("התעודה לא הותקן", "התקנת תעודת אבטחה", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                if (answer == DialogResult.Retry)
+                    fixcert_Click(sender, e);
+            }
+        }
+    }
+}
